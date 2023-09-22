b320513920aab2c29cd4057ce247cf0fa8a7cddc
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-09-22 12:38:27 +0300

    create method

--- progrm/Program.cs
+++ progrm/Program.cs
@@ -4,14 +4,21 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.Write("Укажите длину массива: ");
+int len_AR = int.Parse(Console.ReadLine());
 
 
 
-void CreateArr(string[]){
-
+string[] CreateArr(int l_ar){
+    string[] res = string[l_ar];
+    for (int i = 0; i < l_ar; i++){
+        Console.WriteLine("Введите ")
+    }
 }
 
 
 
+Console.WriteLine(arrar);
+// Console.WriteLine($"[{String.Join(",", array)}]");
 
 
